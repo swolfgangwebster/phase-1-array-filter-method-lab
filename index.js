3905f0f5ef40221330520b09c1a0319e62a08d1c
Shawn Webster <swolfgangwebster@gmail.com>
2023-04-01 19:50:18 -0500

    filter

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+function findMatching(a,s) {
+    return a.filter(i => i.toLowerCase() == s.toLowerCase())
+}
+function fuzzyMatch(a,s) {
+    return a.filter(i => i.slice(0, s.length) == s)
+}
+
+function matchName(a,s) {
+    return a.filter(i => i.name == s)
+}
